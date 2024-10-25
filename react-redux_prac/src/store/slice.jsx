5a9856f223ae6242c2c5dcc6ad353cae5bb91e88
ASI\esebial <esebial@csyoutsourcing..com>
2024-10-25 13:57:37 +0800

    updating

--- react-redux_prac/src/store/slice.jsx
+++ react-redux_prac/src/store/slice.jsx
@@ -33,7 +33,6 @@ export const todoSlice = createSlice({
                 existingTodo.status = "complete";
             }else{
                 existingTodo.status =  "incomplete";
-
             }
         },
 
